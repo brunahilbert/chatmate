67d045f1bfa01b12e0ba33ffc597d1d274c17edb
brunahilbert <bruna.hilbertt@gmail.com>
2023-03-29 17:15:19 +0200

    fix: set responsive space between UIs

--- components/Start.js
+++ components/Start.js
@@ -3,12 +3,12 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
   TextInput,
   ImageBackground,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
-import BackgroundImage from '../img/background-image.png';
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState('');
@@ -16,8 +16,11 @@ const Start = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={BackgroundImage} style={styles.image}>
-        <Text style={styles.appTitle}>Welcome to ChatMate</Text>
+      <ImageBackground
+        source={require('../img/background-image.png')}
+        style={styles.image}
+      >
+        <Text style={styles.appTitle}>Welcome to ChatMate!</Text>
         <View style={styles.box}>
           <TextInput
             style={styles.usernameInput}
@@ -25,35 +28,60 @@ const Start = ({ navigation }) => {
             onChangeText={setName}
             placeholder='Your Name'
           />
-          <Text style={[styles.usernameInput, styles.colorSelector]}>
-            Choose Background Color:
-          </Text>
-          <View style={styles.colorWrapper}>
-            <TouchableOpacity
-              style={[styles.color, styles.black]}
-              onPress={() => setColor('#090C08')}
-            ></TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.color, styles.purple]}
-              onPress={() => setColor('#474056')}
-            ></TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.color, styles.blue]}
-              onPress={() => setColor('#8A95A5')}
-            ></TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.color, styles.green]}
-              onPress={() => setColor('#B9C6AE')}
-            ></TouchableOpacity>
+          <View style={styles.colorSelectorWrapper}>
+            <Text style={[styles.usernameInput, styles.colorSelector]}>
+              Choose Background Color:
+            </Text>
+            <View style={styles.colorWrapper}>
+              <TouchableOpacity
+                accessible={true}
+                accessibilityLabel='Black'
+                accessibilityHint='Lets you choose the chats background color'
+                accessibilityRole='button'
+                style={[styles.color, styles.black]}
+                onPress={() => setColor('#090C08')}
+              ></TouchableOpacity>
+              <TouchableOpacity
+                accessible={true}
+                accessibilityLabel='Purple'
+                accessibilityHint='Lets you choose the chats background color'
+                accessibilityRole='button'
+                style={[styles.color, styles.purple]}
+                onPress={() => setColor('#474056')}
+              ></TouchableOpacity>
+              <TouchableOpacity
+                accessible={true}
+                accessibilityLabel='Light blue'
+                accessibilityHint='Lets you choose the chats background color'
+                accessibilityRole='button'
+                style={[styles.color, styles.blue]}
+                onPress={() => setColor('#8A95A5')}
+              ></TouchableOpacity>
+              <TouchableOpacity
+                accessible={true}
+                accessibilityLabel='Light green'
+                accessibilityHint='Lets you choose the chats background color'
+                accessibilityRole='button'
+                style={[styles.color, styles.green]}
+                onPress={() => setColor('#B9C6AE')}
+              ></TouchableOpacity>
+            </View>
           </View>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => navigation.navigate('Chat', { name: name, color: color})}
+            onPress={() =>
+              navigation.navigate('Chat', { name: name, color: color })
+            }
           >
             <Text style={styles.buttonText}>Start Chatting</Text>
           </TouchableOpacity>
         </View>
       </ImageBackground>
+      {/* Fix the issue on iOS where the keyboard covers almost all of the name 
+          and background color picker form on the Start screen */}
+      {Platform.OS === 'ios' ? (
+        <KeyboardAvoidingView behavior='padding' />
+      ) : null}
     </View>
   );
 };
@@ -74,31 +102,37 @@ const styles = StyleSheet.create({
     fontSize: 45,
     fontWeight: '600',
     color: '#FFFFFF',
-    alignSelf: 'center',
+    textAlign: 'center',
   },
   box: {
     flex: 100,
     marginBottom: 30,
     width: '88%',
     alignSelf: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#FFFFFF',
   },
   usernameInput: {
     width: '88%',
     padding: 15,
     borderWidth: 1,
+    borderColor: '#757083',
     marginTop: 25,
-    marginBottom: 20,
     fontSize: 16,
     fontWeight: '300',
     color: '#757083',
-    opacity: 0.5,
+    opacity: 0.8,
     alignSelf: 'center',
+  },
+  colorSelectorWrapper: {
+    marginVertical: 10,
   },
   colorSelector: {
     opacity: 1,
     borderWidth: 0,
     padding: 0,
+    marginBottom: 15,
+    marginTop: 0,
   },
   colorWrapper: {
     flexDirection: 'row',
@@ -108,7 +142,6 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
-    marginBottom: 45,
   },
   black: {
     backgroundColor: '#090C08',
@@ -125,6 +158,7 @@ const styles = StyleSheet.create({
   button: {
     width: '88%',
     padding: 20,
+    marginBottom: 25,
     backgroundColor: '#757083',
     alignItems: 'center',
     alignSelf: 'center',
@@ -133,7 +167,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
     color: '#FFFFFF',
-  }
+  },
 });
 
 export default Start;
