5e18bce2ea03ec9580e7a45166fef57c92ffb25f
brunahilbert <bruna.hilbertt@gmail.com>
2023-03-31 16:16:32 +0200

    connect app with firestore

--- App.js
+++ App.js
@@ -4,15 +4,33 @@ import Start from './components/Start';
 import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD0v6CrAIurHvI0sUhypcnxtXf-0oBv0cM",
+    authDomain: "chatmate-app-demo.firebaseapp.com",
+    projectId: "chatmate-app-demo",
+    storageBucket: "chatmate-app-demo.appspot.com",
+    messagingSenderId: "482813963044",
+    appId: "1:482813963044:web:de37cc0cb1375d97bf21b1"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
         <Stack.Screen name='Start' component={Start} />
-        <Stack.Screen name='Chat' component={Chat} />
+        <Stack.Screen name='Chat'>
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
